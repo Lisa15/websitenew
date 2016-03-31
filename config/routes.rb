14f88c5f6c8ca6eb8e66912a3be461028b61fa54
Lisa <lisa@crystalair.ie>
2016-03-31 15:53:30 +0000

    First Section Offers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +14,8 @@ Rails.application.routes.draw do
   get 'partners', to: 'welcome#partners'
    get 'ourteam', to: 'welcome#ourteam'
   
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
