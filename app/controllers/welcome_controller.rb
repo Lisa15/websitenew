ec93b64ef1bd1e505839a9a3f79ecca6dc1748b3
Lisa <lisa@crystalair.ie>
2016-04-03 21:01:33 +0000

    Login setup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,28 +1,28 @@
 class WelcomeController < ApplicationController
-
+skip_before_action :authenticate_user!, only: [:home]
   def home
 
   end
-  
+
   def aboutus
-    
+
   end
-  
+
   def services
-    
+
   end
-  
+
   def projects
-    
+
   end
-  
+
   def partners
-    
+
   end
-  
+
   def ourteam
-    
+
   end
-    
-    
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
   get 'partners', to: 'welcome#partners'
    get 'ourteam', to: 'welcome#ourteam'
   
-
+ devise_for :users
 
 
   # Example of regular route:
